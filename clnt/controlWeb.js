c563b738a590b6956d38125bdf4bec3ddbaf4639
Illán <illan.martinez@hotmail.com>
2024-05-06 19:34:41 +0200

    Menú

--- clnt/controlWeb.js
+++ clnt/controlWeb.js
@@ -1,15 +1,24 @@
-function ControlWeb(){
-    this.init = function(){
+function ControlWeb() {
+    this.init = function () {
         $("#au").empty();
-        $("#au").load("./clnt/login.html",function(){
-            console.log("R");
-            $("#iniSesion").click(function(){
-                event.preventDefault();
-                var user = $("#email").val();
-                var pass = $("#password").val();
-                cr.iniciarSesion(user, pass);
-            })
-        });
-
-    }
+        if ($.cookie("email") == undefined) {
+            $("#au").load("./clnt/login.html", function () {
+                $("#iniSesion").click(function () {
+                    event.preventDefault();
+                    var user = $("#email").val();
+                    var pass = $("#password").val();
+                    cr.iniciarSesion(user, pass);
+                });
+            });
+        } else {
+            $("#au").load("./clnt/menu.html", function () {
+                $("#cerrarSesion").click(function () {
+                    event.preventDefault();
+                    $.removeCookie("email");
+                    $.removeCookie("tkn");
+                    location.reload();
+                });
+            });
+        }
+    };
 }
