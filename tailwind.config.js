5802bd0577f092db2bc2ba0a7f471080b3e1842a
Illán <illan.martinez@hotmail.com>
2024-05-24 00:12:03 +0200

    Css Front

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./clnt/**/*.{html,js}"],
+  content: [
+    "./clnt/**/*.{html,js}",  // Incluye todos los archivos HTML y JS en la carpeta clnt
+  ],
   theme: {
     extend: {
       colors: {
@@ -9,8 +11,8 @@ module.exports = {
           200: '#c7d2fe',
           300: '#a5b4fc',
           400: '#818cf8',
-          500: '#6366f1', // Usualmente usado como el color base
-          600: '#4f46e5', // Tu color personalizado puede ser el base o cualquier otro tono
+          500: '#6366f1',
+          600: '#4f46e5',
           700: '#4338ca',
           800: '#3730a3',
           900: '#312e81'
@@ -19,7 +21,12 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('daisyui'),
   ],
-}
-
+  daisyui: {
+    styled: true,
+    themes: true,
+    prefix: 'dui_',
+  }
+};
